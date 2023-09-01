d8eeef55b5457d9b49d751fa7158002befe86a17
marcobusacca <59514839+marcobusacca@users.noreply.github.com>
2023-09-01 17:01:24 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!-- JAVASCRIPT & VUE.JS -->
+<script>
+import { store } from '../store';
+
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    components: {
+        AppHeader,
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+    <AppHeader/>
+    <main>
+        <router-view></router-view>
+    </main>
+</template>
+
+<!-- STYLE SCSS -->
+<style lang="scss">
+// IMPORTO GENERALS.SCSS
+@use './styles/generals.scss' as *;
+</style>
