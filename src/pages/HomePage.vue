21f79c5ddd2d7ddddbb641a8ab66db1bce66b81b
andreathepa <andreadepa32@gmail.com>
2023-09-01 18:35:41 +0200

    [STYLE] Styling homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -11,10 +11,58 @@ export default {
 
 <!-- TEMPLATE HTML -->
 <template lang="">
-    <div class="container mb-5">
+    <div class="container-fluid jumbotron p-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-8 my-5 text-white">
+                        <h3>Il server gestionale che si adatta alle esigenze <br> dei tuoi clienti a quattro zampe</h3>
+                    </div>
+                    <div class="col-4">
+                        <img src="../../public/img/bulldog.png" class="img-bulldog" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container p-5">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-6 my-5">
+                        <h3>Un software gestionale veterinario basato sul cloud può sembrare un grande cambiamento, ma noi siamo qui per aiutarti.</h3>
+                        <p>L'obiettivo di Provet Cloud è quello di aiutarti nella gestione e nella produttività del tuo studio veterinario. Il nostro team di implementazione e migrazione, composto da professionisti del settore veterinario, sa perfettamente come semplificarti la vita. Se decidi di passare al Cloud per risparmiare tempo e denaro, ti mostreremo come ottenere il massimo dal tuo nuovo software veterinario.</p>
+                    </div>
+                    <div class="col-6">
+                        immagine qui
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid jumbotron p-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-12 my-5 text-white">
+                        <h3>"Il punto di forza di Provet Cloud è che si preoccupano di capire come funziona la mia attività. Ho scelto loro perché si sono fermati ad ascoltarmi e da lì è cominciato il percorso <br> con Provet Cloud".</h3>
+                        <p>Dr. Tom Walsh di PrimePet,  Centro di terapia e riabilitazione veterinaria.</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container p-5">
         <div class="row">
-            <div class="col-12 text-center my-5">
-                <h1>Homepage</h1>
+            <div class="col-12">
+                <h2>Perché Boolpetcare?</h2>
+                <p>Provet Cloud offre soluzioni e funzionalità per cliniche di ogni dimensione e specialità. Dai piccoli studi, agli ambulatori e alle università, Provet Cloud è utilizzato da professionisti veterinari in tutto il mondo.</p>
+                <p><strong>Investi più tempo nei tuoi pazienti e smetti di sprecarlo nella gestione del tuo centro veterinario. Aumenta l'efficienza della tua clinica grazie a integrazioni e funzionalità che permettono di relazionarti meglio con i tuoi clienti, di automatizzare le attività e la gestione degli orari e risparmiare energie e risorse.</strong></p>
+                <div>
+                    <h1>QUI METTERE LO SLIDER DELLE FUNZIONALITà</h1>
+                </div>
             </div>
         </div>
     </div>
