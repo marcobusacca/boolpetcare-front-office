b2ab68736d41e0a5cbc4b8122d9db4e4a0913691
andreathepa <andreadepa32@gmail.com>
2023-09-02 12:16:36 +0200

    style homepage

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -21,6 +21,19 @@ export default {
 <!-- TEMPLATE HTML -->
 <template lang="">
     <footer>
+        <section class="container-fluid p-3 bg-dark">
+            <div class="container my-3">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="content-footer text-center text-white my-3 fs-7">
+                            <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam tempora vero natus aspernatur, quaerat, omnis et quibusdam repudiandae exercitationem. Illo?</p>
+                            <p class="">Lorem ipsum <span><a class="text-white" href="">dolor et amen</a></span> dolor sit amet consectetur adipisicing elit.</p>
+                            <p class="">© Low Quality Software 2023. <span><a class="text-white" href="">Privacy Policy</a></span></p>
+                        </div>
+                    </div>                        
+                </div>
+            </div>
+        </section>
     </footer>
 </template>
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -11,57 +11,70 @@ export default {
 
 <!-- TEMPLATE HTML -->
 <template lang="">
-    <div class="container-fluid jumbotron p-5">
-        <div class="row justify-content-center">
-            <div class="col-8">
-                <div class="row">
-                    <div class="col-8 my-5 text-white">
-                        <h3>Il server gestionale che si adatta alle esigenze <br> dei tuoi clienti a quattro zampe</h3>
+    <div class="container-fluid jumbotron p-3">
+        <div class="container p-3">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-8 my-5">
+                    <div class="content text-white text-center text-md-start">
+                        <h3>Il server gestionale che si adatta alle esigenze dei tuoi clienti a quattro zampe</h3>
+    
                     </div>
-                    <div class="col-4">
+                </div>
+                <div class="col-12 col-md-4 col-lg-4">
+                    <div class="content-img">
                         <img src="../../public/img/bulldog.png" class="img-bulldog" alt="">
+    
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="container p-5">
+    <div class="container p-3 mt-3">
         <div class="row justify-content-center">
-            <div class="col-12">
-                <div class="row">
-                    <div class="col-4">
-                        immagine qui
-                    </div>
-                    <div class="col-8 my-5">
-                        <h3>Un software gestionale veterinario basato sul cloud può sembrare un grande cambiamento, ma noi siamo qui per aiutarti.</h3>
-                        <p>L'obiettivo di BoolPetCare è quello di aiutarti nella gestione e nella produttività del tuo studio veterinario. Il nostro team di implementazione e migrazione, composto da professionisti del settore veterinario, sa perfettamente come semplificarti la vita. Se decidi di passare al Cloud per risparmiare tempo e denaro, ti mostreremo come ottenere il massimo dal tuo nuovo software veterinario.</p>
-                    </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="content-img p-3">
+                    <img src="../../public/img/homepage-desktop.png" class="img-desktop" alt="">
+               </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="content text-center text-md-start my-5">
+                    <h3>Un software gestionale veterinario che renderà il tuo lavoro una passeggiata.</h3>
+                    <p class="my-4">L'obiettivo di BoolPetCare è quello di aiutarti nella gestione e nella produttività del tuo studio veterinario. Il nostro team di sviluppatori si è soffermato ad ascoltare le esigenze dei professionisti del settore per creare un prodotto che si adegua perfettamente alle tue necessità.</p>
+                    <button type="button" class="btn button-demo my-3 text-white">Prova demo</button>
                 </div>
             </div>
         </div>
     </div>
-    <div class="container-fluid jumbotron p-5">
-        <div class="row justify-content-center">
-            <div class="col-8">
-                <div class="row">
-                    <div class="col-12 my-5 text-white">
-                        <h3>"Il punto di forza di BoolPetCare è che si preoccupano di capire come funziona la mia attività. Ho scelto loro perché si sono fermati ad ascoltarmi e da lì è cominciato il percorso <br> con BoolPetCare".</h3>
-                        <p>Dr. Tom Walsh di PrimePet,  Centro di terapia e riabilitazione veterinaria.</p>
+    <div class="container-fluid jumbotron p-3">
+        <div class="container p-3">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="content text-center text-lg-start my-5 text-white">
+                        <h3>"Il punto di forza di BoolPetCare è che si preoccupano di capire come funziona la mia attività. Ho scelto loro perché si sono fermati ad ascoltarmi e da lì è cominciato il percorso con BoolPetCare".</h3>
+                        <p class="my-4">Dr. Filippo Baudo di BoolPetCare, Centro di terapia e riabilitazione veterinaria.</p>
                     </div>
-                    
                 </div>
             </div>
+
         </div>
     </div>
     <div class="container p-5">
         <div class="row">
-            <div class="col-12 my-5">
-                <h2>Perché BoolPetCare?</h2>
-                <p>BoolPetCare offre soluzioni e funzionalità per cliniche di ogni dimensione e specialità. Dai piccoli studi, agli ambulatori e alle università, BoolPetCare è utilizzato da professionisti veterinari in tutto il mondo.</p>
-                <p><strong>Investi più tempo nei tuoi pazienti e smetti di sprecarlo nella gestione del tuo centro veterinario. Aumenta l'efficienza della tua clinica grazie a integrazioni e funzionalità che permettono di relazionarti meglio con i tuoi clienti, di automatizzare le attività e la gestione degli orari e risparmiare energie e risorse.</strong></p>
+            <div class="col-12">
+                <div class="content text-center text-lg-start my-5">
+                    <h2>Perché BoolPetCare?</h2>
+                    <p>BoolPetCare offre soluzioni e funzionalità per cliniche di ogni dimensione e specialità. Dai piccoli studi, agli ambulatori e alle università, BoolPetCare è utilizzato da professionisti veterinari in tutto il mondo.</p>
+                    <p><strong>Investi più tempo nei tuoi pazienti e smetti di sprecarlo nella gestione del tuo centro veterinario. Aumenta l'efficienza della tua clinica grazie a integrazioni e funzionalità che permettono di relazionarti meglio con i tuoi clienti, di automatizzare le attività e la gestione degli orari e risparmiare energie e risorse.</strong></p>
+
+                </div>
             </div>
-            <div class="col-12 text-center py-5 my-5">
-                <h2>Scopri le Funzionalità di BoolPetCare</h2>
+            <div class="col-12">
+                <div class="content text-center text-lg-start my-2">
+                    <button type="button" class="btn button-demo p-3 text-white">
+                        <h2>Scopri le Funzionalità di BoolPetCare</h2>
+                    </button>
+
+                </div>
             </div>
         </div>
     </div>
